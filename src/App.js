d9da81bfedd57b20f4491afe57d9d41538ceb02d
sofiadb <barreirosofia@hotmail.com>
2019-07-20 20:03:06 -0300

    #3 commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import './App.css';
 
-function Form1(props) {
 
-  return (
-    <form action="">
-      <table id='table1'>
-        <tr colspan='3'><td>{props.mytext}</td></tr>
-        <tr colspan='3'><td>{props.myText2}</td></tr>
-        <tr><td><select name="" id="select1">
-          <option value="Capitalización simple">Capitalización simple</option>
-          <option value="Capitalización compuesta">Capitalización compuesta</option>
-          <option value="Descuento">Descuento</option>
-          <option value="Tasa efectiva">Tasa efectiva</option>
-        </select></td></tr>
-      </table>
-    </form>);
 
+class Form1 extends React.Component {
+  state = {
+    show: true
+  }
+    render(){
+      if(this.state.show){
+      return(
+        <form action="">
+          <table id='table1' >
+            <tr colspan='3'><td>{this.props.mytext}</td></tr>
+            <tr colspan='3'><td>{this.props.myText2}</td></tr>
+            <tr><td><select onChange={()=>{
+              console.log('cambio')
+              }}
+              name="" id="select1">
+              <option value="Capitalización simple">Capitalización simple</option>
+              <option value="Capitalización compuesta">Capitalización compuesta</option>
+              <option value="Descuento">Descuento</option>
+              <option value="Tasa efectiva">Tasa efectiva</option>
+            </select></td></tr>
+          </table>
+        </form>
+      )
+    }
+  }
 }
 
-class App extends React.Component{
 
-  render(){
-  return <div>this is my  componet; <Form1 mytext= "Bienvenido a la Calculadora Financiera" myText2 = 'Cuál es la operación que desea realizar?' /></div>
+class App extends React.Component {
+
+  render() {
+    return <div>this is my  component
+    <Form1 mytext="Bienvenido a la Calculadora Financiera"
+        myText2='Cuál es la operación que desea realizar?' />
+    </div>
 
   }
 }
